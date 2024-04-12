e0c83d22318bb34550bb80fe1f02df615b8878d2
Joel Pita Vidal <jpitavidal@danielcastelao.org>
2024-04-12 12:10:36 +0200

    Layout Calculadora

---
+++ LayoutCalculadora/src/LayoutCalculadora.java
@@ -0,0 +1,8 @@
+public class LayoutCalculadora{
+
+    public static void main(String[] args){
+        Calculadora calculadora = new Calculadora();
+        calculadora.setVisible(true);
+    }
+
+}
